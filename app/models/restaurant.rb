616a443beb6646ba21d23cb0fdec6dad6bb4b74e
Artur Pedrosa <maverickanp@gmail.com>
2019-11-21 12:07:03 -0300

    Views, seeds, controller

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ApplicationRecord
+  CATEGORY = %w(chinese italian japanese french belgian)
+  validates :name, presence: true
+  validates :category, inclusion: { in: %w() }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+# Check for the environment
+if Rails.env.development?
+  Restaurant.destroy_all
+end
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Tlinta e Tlês',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '12345678',
+    category:        'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '987654321',
+    category:        'belgium'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
